c0b94bf259e39cf2e70579909e0e94830e58bc29
Grigoriy Popov <grisha_mgn@bk.ru>
2024-07-25 21:34:31 +0500

    update test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Aleksandr');
+        expect(container.textContent).toBe('AboutAuthor: AleksandrInspector: Grigorii');
     });
 });
